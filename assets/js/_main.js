90e6841139c417eb51d50d24b18898b659c46a3a
Jonas Friedmann <j@frd.mn>
2014-10-18 01:13:24 +0200

    Base JS

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-    console.log("jQuery loaded");
+	if(window.location.hash) {
+		// Replace span with hash ...
+		$('span').text('Our ' + window.location.hash.substring(1));
+		// ... and substitute title
+		$('title').text(window.location.hash.substring(1) + ' - ' + $('title').text());
+	}
 });
-
-console.log("DOM loaded");
