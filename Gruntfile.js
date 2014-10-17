13060480efa002244ef713beae5526039446f25a
Jonas Friedmann <j@frd.mn>
2014-10-18 01:12:18 +0200

    Embed jQuery

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,6 +63,7 @@ module.exports = function(grunt) {
             },
             dist: {
                 src: [
+                    '<%= dirs.bower %>/jquery/dist/jquery.js',
                     '<%= dirs.js %>/*.js',
                     '!<%= dirs.js %>/modernizr.js',
                     '!<%= dirs.js %>/build.js'
