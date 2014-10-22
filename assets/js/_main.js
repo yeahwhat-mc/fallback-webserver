4840961b335160f70936d23f162044440e72681f
Jonas Friedmann <j@frd.mn>
2014-10-22 13:17:07 +0200

    Function to check status of an URL via jQuery

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,8 +1,33 @@
+var subdomain, fulldomain;
+
+function checkURL(url) {
+  $.ajax('http://cors-anywhere.herokuapp.com/' + url, {
+    statusCode: {
+      404: function() {
+        console.log('Not working: ' + url);
+      },
+      200: function() {
+        console.log('Working: ' + url);
+      }
+    }
+  });
+}
+
 $(document).ready(function(){
-	if(window.location.hash) {
-		// Replace span with hash ...
-		$('span').text('Our ' + window.location.hash.substring(1));
-		// ... and substitute title
-		$('title').text(window.location.hash.substring(1) + ' - ' + $('title').text());
-	}
+  if(window.location.hash) {
+    subdomain = window.location.hash.substring(1);
+  } else {
+    subdomain = 'www';
+  }
+
+  fulldomain = subdomain + '.yeahwh.at';
+
+  if (subdomain != 'www') {
+    // Replace span with hash ...
+    $('span').text('Our ' + subdomain);
+    // ... and substitute title
+    $('title').text(subdomain + ' - ' + $('title').text());
+  }
+
+  setInterval(checkURL('http://' + fulldomain), 5 * 1000);
 });
